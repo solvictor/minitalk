401c536ad17f044c609f8e31d99377a39a9d66df
Victor Egret <victor.egret.pro@gmail.com>
2022-12-24 23:58:02 +0100

    Support for unicode characters

--- sources/client.c
+++ sources/client.c
@@ -6,48 +6,44 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 19:30:55 by vegret            #+#    #+#             */
-/*   Updated: 2022/12/24 01:32:31 by vegret           ###   ########.fr       */
+/*   Updated: 2022/12/24 23:35:37 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	ft_atoi(const char *s)
+static int	parse_pid(const char *s)
 {
-	long	result;
-	int		sign;
+	long	pid;
 	int		i;
 
-	result = 0;
+	pid = 0;
 	i = 0;
-	while (s[i] == 32 || (s[i] > 8 && s[i] < 14))
-		i++;
-	sign = 1;
-	if (s[i] == '-' || s[i] == '+')
-		if (s[i++] == '-')
-			sign = -1;
-	while (s[i] >= '0' && s[i] <= '9')
+	while (s[i])
 	{
-		if (result > (LONG_MAX / 10))
-			return (-1 * (sign == 1));
-		result = result * 10 + s[i++] - 48;
-		if (result < 0)
-			return (-1 * (sign == 1));
+		if (s[i] < '0' || s[i] > '9')
+			return (-1);
+		pid = pid * 10 + s[i] - '0';
+		i++;
 	}
-	return (result * sign);
+	return (pid);
 }
 
-int	send_char(pid_t pid, char c)
+int	send_char(pid_t pid, unsigned char c)
 {
 	int	i;
+	int	response;
 
 	i = 0;
 	while (i < 8)
 	{
-		if (c & (1 << i))
-			kill(pid, SIGUSR1);
+		if (c % 2)
+			response = kill(pid, SIGUSR1);
 		else
-			kill(pid, SIGUSR2);
+			response = kill(pid, SIGUSR2);
+		if (response == -1)
+			return (1);
+		c >>= 1;
 		i++;
 		usleep(10);
 	}
@@ -58,9 +54,10 @@ int	send_message(pid_t pid, char *message)
 {
 	while (*message)
 	{
-		if (send_char(pid, *message))
-			return (1);
+		if (send_char(pid, (unsigned char) *message))
+			return (ft_printf("Failed to send character\n"), 1);
 		message++;
+		usleep(10);
 	}
 	return (0);
 }
@@ -71,8 +68,8 @@ int	main(int argc, char *argv[])
 
 	if (argc != 3)
 		return (ft_printf("Usage: %s <Servers's PID> <Message>\n", argv[0]), 1);
-	pid = ft_atoi(argv[1]);
-	if (pid < 1 || kill(pid, 0) != 0)
+	pid = parse_pid(argv[1]);
+	if (pid < 1 || kill(pid, 0) == -1)
 		return (ft_printf("Invalid PID\n"), 1);
 	return (send_message(pid, argv[2]));
 }
