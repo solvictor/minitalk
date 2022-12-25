03e96997d9c19d432602d0410dad1beffbf58948
Victor Egret <victor.egret.pro@gmail.com>
2022-12-25 01:46:28 +0100

    Removed unsused includes and SIGINT handling

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,17 +6,15 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 19:25:37 by vegret            #+#    #+#             */
-/*   Updated: 2022/12/23 23:35:33 by vegret           ###   ########.fr       */
+/*   Updated: 2022/12/25 01:45:23 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include	<sys/types.h>
 # include	<unistd.h>
 # include	<signal.h>
-# include	<limits.h>
 # include	"ft_printf.h"
 
 #endif
--- sources/server.c
+++ sources/server.c
@@ -6,18 +6,11 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 19:47:08 by vegret            #+#    #+#             */
-/*   Updated: 2022/12/24 23:38:30 by vegret           ###   ########.fr       */
+/*   Updated: 2022/12/25 01:46:00 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-void	stop_server(int sig)
-{
-	(void) sig;
-	ft_printf("\nServer stopped by SIGINT\n");
-	exit(0);
-}
 
 void	receive(int sig)
 {
@@ -40,8 +33,6 @@ int	main(void)
 		return (ft_printf("Failed SIGUSR1 handling"), 1);
 	if (signal(SIGUSR2, &receive) == SIG_ERR)
 		return (ft_printf("Failed SIGUSR2 handling"), 1);
-	if (signal(SIGINT, &stop_server) == SIG_ERR)
-		return (ft_printf("Failed SIGINT handling"), 1);
 	ft_printf("Server's PID: %d\n", getpid());
 	while (1)
 		pause();
